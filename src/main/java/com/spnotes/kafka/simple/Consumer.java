eaa238bfca52c6b161c8a7b882f6aaff0bd13365
Prasannakumar Potnuru <S537361@nwmissouri.edu>
2020-10-02 16:18:45 -0500

    Created Consumer

--- src/main/java/com/spnotes/kafka/simple/Consumer.java
+++ src/main/java/com/spnotes/kafka/simple/Consumer.java
@@ -1,12 +1,12 @@
-package com.spnotes.kafka.partition;
+package com.spnotes.kafka.simple;
 
-import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -15,9 +15,8 @@ import java.util.Scanner;
  */
 public class Consumer {
     private static Scanner in;
-    private static boolean stop = false;
 
-    public static void main(String[] argv) throws Exception {
+    public static void main(String[] argv)throws Exception{
         if (argv.length != 2) {
             System.err.printf("Usage: %s <topicName> <groupId>\n",
                     Consumer.class.getSimpleName());
@@ -27,44 +26,37 @@ public class Consumer {
         String topicName = argv[0];
         String groupId = argv[1];
 
-        ConsumerThread consumerThread = new ConsumerThread(topicName, groupId);
-        consumerThread.start();
+        ConsumerThread consumerRunnable = new ConsumerThread(topicName,groupId);
+        consumerRunnable.start();
         String line = "";
         while (!line.equals("exit")) {
             line = in.next();
         }
-        consumerThread.getKafkaConsumer().wakeup();
+        consumerRunnable.getKafkaConsumer().wakeup();
         System.out.println("Stopping consumer .....");
-        consumerThread.join();
+        consumerRunnable.join();
     }
 
-    private static class ConsumerThread extends Thread {
+    private static class ConsumerThread extends Thread{
         private String topicName;
         private String groupId;
-        private KafkaConsumer<String, String> kafkaConsumer;
+        private KafkaConsumer<String,String> kafkaConsumer;
 
-        public ConsumerThread(String topicName, String groupId) {
+        public ConsumerThread(String topicName, String groupId){
             this.topicName = topicName;
             this.groupId = groupId;
         }
-
         public void run() {
             Properties configProperties = new Properties();
             configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-            configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+            configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer");
             configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
             configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
 
             //Figure out where to start processing messages from
             kafkaConsumer = new KafkaConsumer<String, String>(configProperties);
-            kafkaConsumer.subscribe(Arrays.asList(topicName), new ConsumerRebalanceListener() {
-                public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-                    System.out.printf("%s topic-partitions are revoked from this consumer\n", Arrays.toString(partitions.toArray()));
-                }
-                public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-                    System.out.printf("%s topic-partitions are assigned to this consumer\n", Arrays.toString(partitions.toArray()));
-                }
-            });
+            kafkaConsumer.subscribe(Arrays.asList(topicName));
             //Start processing messages
             try {
                 while (true) {
@@ -72,16 +64,15 @@ public class Consumer {
                     for (ConsumerRecord<String, String> record : records)
                         System.out.println(record.value());
                 }
-            } catch (WakeupException ex) {
+            }catch(WakeupException ex){
                 System.out.println("Exception caught " + ex.getMessage());
-            } finally {
+            }finally{
                 kafkaConsumer.close();
                 System.out.println("After closing KafkaConsumer");
             }
         }
-
-        public KafkaConsumer<String, String> getKafkaConsumer() {
-            return this.kafkaConsumer;
+        public KafkaConsumer<String,String> getKafkaConsumer(){
+           return this.kafkaConsumer;
         }
     }
 }
