7a8e29159154b7acaf9f38ba14e6921e75e73425
yugimamidi <yugi.mamidi01@gmail.com>
2020-10-02 20:44:30 -0500

    updated with prime numbers logic

--- src/main/java/edu/nwmissouri/kafkabigdatagroupapp/simple/MamidiProducer.java
+++ src/main/java/edu/nwmissouri/kafkabigdatagroupapp/simple/MamidiProducer.java
@@ -4,41 +4,68 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-
-
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Created by sunilpatil on 12/28/15.
- */
 public class MamidiProducer {
-    private static Scanner in;
-    public static void main(String[] argv)throws Exception {
-        if (argv.length != 1) {
-            System.err.println("Please specify 1 parameters ");
-            System.exit(-1);
-        }
-        String topicName = argv[0];
-        in = new Scanner(System.in);
-        System.out.println("Enter message(type exit to quit)");
+  private static Scanner in;
+
+  public static void main(String[] argv) throws Exception {
+    if (argv.length != 1) {
+      System.err.println("Please specify 1 parameter (the name of the topic)");
+      System.exit(-1);
+    }
+    String topicName = argv[0];
+    in = new Scanner(System.in);
+    System.out.println("Enter message (type exit to quit).\n");
+
+    // Configure the Producer
+    Properties configProperties = new Properties();
+    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.ByteArraySerializer");
+    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    System.out.println("The configuration properties are: " + configProperties.toString());
+    System.out.println("\nWill use this configuration to create a producer.\n");
+
+    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+
+    
+        String printnum = printnumbers();
+        ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, printnum);
+        producer.send(rec);
     
 
-        //Configure the Producer
-        Properties configProperties = new Properties();
-        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.ByteArraySerializer");
-        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+    in.close();
+    producer.close();
 
-        org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
-        String line = in.nextLine();
-        while(!line.equals("exit")) {
-            //TODO: Make sure to use the ProducerRecord constructor that does not take parition Id
-            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,line);
-            producer.send(rec);
-            line = in.nextLine();
-        }
-        in.close();
-        producer.close();
-    }
+  }
+
+  private static String printnumbers(){
+     int n=10;
+      int status = 1;
+      int num = 3;
+      String x=" ";
+      for ( int i = 2 ; i <=n ;  )
+      {
+         for ( int j = 2 ; j <= Math.sqrt(num) ; j++ )
+         {
+            if ( num%j == 0 )
+            {
+               status = 0;
+               break;
+            }
+         }
+         if ( status != 0 )
+         {
+            x=x+" \n "+num+" \n ";
+            i++;
+         }
+         status = 1;
+         num++;
+      }         
+        return "The list of prime numbers are: \n"+x;
+  }
 }
